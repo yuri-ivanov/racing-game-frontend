d6c1c21614507634c3917181975208e726eef79b
Yuri Ivanov <yura.ivanov@gmail.com>
2019-05-20 16:24:44 +0200

    Update game.tsx

--- src/game.tsx
+++ src/game.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {Game, GameDataSource} from './GameDataSource';
 import { RouteComponentProps } from 'react-router-dom';
+import { resolve } from 'dns';
+import { reject } from 'q';
 
 interface GameProps extends RouteComponentProps<any> {
 }
 
 interface GameComponentState{
+    loading: boolean;
     game: Game | null
 }
 
@@ -13,28 +16,35 @@ export class GameComponent extends React.Component<GameProps, GameComponentState
 
     constructor(props: GameProps){
         super(props);
-        this.state = {game: null}; 
+        this.state = {game: null, loading: false}; 
     }
 
 
     loadGame(gameId:string):Promise<Game> {
+        if(this.state.loading){
+            console.log('loading game is in progress');
+            return new Promise((resolve, reject) => {
+                reject("loading game is in progress");
+            });
+        }
+        this.setState({loading: true});
         return GameDataSource.getGame(gameId)
-            .then(jsonData => {this.setState({game: jsonData}); return jsonData;});
+            .then(jsonData => {
+                this.setState({game: jsonData, loading: false}); 
+                return jsonData;});
     }
 
     render() {
         const {params} = this.props.match;
-        if( params.id ){
-            if(!this.state.game || (this.state.game.id !==params.id)) {
-                this.loadGame(params.id);
-            }
+        if(this.state.loading){
+            return (<div>Loading...</div>);
         }
 
         if(!this.state || !this.state.game){
             return null;
         }
         const game = this.state.game;
-        const races = game.races.map(r => <div key={r.name+r.date}>{r.name}, {r.scheduledStartTime}</div>);
+        const races = game.races.map(r => <div key={r.name+r.date.toLocaleDateString()}>{r.name}, {r.scheduledStartTime.toLocaleString()}</div>);
         return (
         <div>
             <div>game id:{game.id}</div>
