c7ae5ab79d9c71c07b3e4bbe173c45b4645e4115
Yuri Ivanov <yura.ivanov@gmail.com>
2019-05-20 12:18:09 +0200

    Create products-v4.js

---
+++ src/json-mock/products-v4.js
@@ -0,0 +1,225 @@
+const PRODUCTS_V4={
+    "betType": "V4",
+    "upcoming": [
+        {
+            "id": "V4_2019-05-20_18_1",
+            "startTime": "2019-05-20T12:20:00",
+            "tracks": [
+                {
+                    "id": 18,
+                    "name": "Halmstad"
+                }
+            ],
+            "favorites": [
+                {
+                    "raceId": "2019-05-20_18_3",
+                    "legNumber": 3,
+                    "start": {
+                        "number": 5,
+                        "name": "Youngblood Pellini",
+                        "distribution": 4200
+                    }
+                },
+                {
+                    "raceId": "2019-05-20_18_1",
+                    "legNumber": 1,
+                    "start": {
+                        "number": 6,
+                        "name": "Cash Flow",
+                        "distribution": 3800
+                    }
+                },
+                {
+                    "raceId": "2019-05-20_18_4",
+                    "legNumber": 4,
+                    "start": {
+                        "number": 1,
+                        "name": "Niky Flax",
+                        "distribution": 3200
+                    }
+                }
+            ]
+        },
+        {
+            "id": "V4_2019-05-20_54_1",
+            "startTime": "2019-05-20T17:00:00",
+            "tracks": [
+                {
+                    "id": 54,
+                    "name": "Odense"
+                }
+            ],
+            "favorites": [
+                {
+                    "raceId": "2019-05-20_54_3",
+                    "legNumber": 3,
+                    "start": {
+                        "number": 15,
+                        "name": "Classico",
+                        "distribution": 5000
+                    }
+                },
+                {
+                    "raceId": "2019-05-20_54_2",
+                    "legNumber": 2,
+                    "start": {
+                        "number": 4,
+                        "name": "Diamant Østervang",
+                        "distribution": 3200
+                    }
+                },
+                {
+                    "raceId": "2019-05-20_54_4",
+                    "legNumber": 4,
+                    "start": {
+                        "number": 7,
+                        "name": "VM Viking Cup",
+                        "distribution": 3100
+                    }
+                }
+            ]
+        },
+        {
+            "id": "V4_2019-05-20_22_6",
+            "startTime": "2019-05-20T20:14:00",
+            "tracks": [
+                {
+                    "id": 22,
+                    "name": "Mantorp"
+                }
+            ],
+            "favorites": [
+                {
+                    "raceId": "2019-05-20_22_8",
+                    "legNumber": 3,
+                    "start": {
+                        "number": 7,
+                        "name": "Jaques Noir",
+                        "distribution": 5800
+                    }
+                },
+                {
+                    "raceId": "2019-05-20_22_6",
+                    "legNumber": 1,
+                    "start": {
+                        "number": 4,
+                        "name": "Ritz Guiness",
+                        "distribution": 4600
+                    }
+                },
+                {
+                    "raceId": "2019-05-20_22_9",
+                    "legNumber": 4,
+                    "start": {
+                        "number": 1,
+                        "name": "Barista Oak",
+                        "distribution": 3000
+                    }
+                }
+            ]
+        },
+        {
+            "id": "V4_2019-05-20_15_6",
+            "startTime": "2019-05-20T20:22:00",
+            "tracks": [
+                {
+                    "id": 15,
+                    "name": "Färjestad"
+                }
+            ],
+            "favorites": [
+                {
+                    "raceId": "2019-05-20_15_8",
+                    "legNumber": 3,
+                    "start": {
+                        "number": 6,
+                        "name": "Remain in Light",
+                        "distribution": 6900
+                    }
+                },
+                {
+                    "raceId": "2019-05-20_15_7",
+                    "legNumber": 2,
+                    "start": {
+                        "number": 6,
+                        "name": "Bol Brage",
+                        "distribution": 4600
+                    }
+                },
+                {
+                    "raceId": "2019-05-20_15_9",
+                    "legNumber": 4,
+                    "start": {
+                        "number": 9,
+                        "name": "Försteslåtten",
+                        "distribution": 2900
+                    }
+                }
+            ]
+        }
+    ],
+    "results": [
+        {
+            "id": "V4_2019-05-19_21_1",
+            "tracks": [
+                {
+                    "id": 21,
+                    "name": "Lindesberg"
+                }
+            ],
+            "startTime": "2019-05-19T11:10:35"
+        },
+        {
+            "id": "V4_2019-05-19_34_4",
+            "tracks": [
+                {
+                    "id": 34,
+                    "name": "Jägersro Galopp"
+                }
+            ],
+            "startTime": "2019-05-19T13:28:51"
+        },
+        {
+            "id": "V4_2019-05-19_54_4",
+            "tracks": [
+                {
+                    "id": 54,
+                    "name": "Ålborg"
+                }
+            ],
+            "startTime": "2019-05-19T13:55:59"
+        },
+        {
+            "id": "V4_2019-05-19_43_6",
+            "tracks": [
+                {
+                    "id": 43,
+                    "name": "Vaggeryd"
+                }
+            ],
+            "startTime": "2019-05-19T15:59:15"
+        },
+        {
+            "id": "V4_2019-05-19_31_7",
+            "tracks": [
+                {
+                    "id": 31,
+                    "name": "Årjäng"
+                }
+            ],
+            "startTime": "2019-05-19T16:06:33"
+        },
+        {
+            "id": "V4_2019-05-19_88_9",
+            "tracks": [
+                {
+                    "id": 88,
+                    "name": "Berlin"
+                }
+            ],
+            "startTime": "2019-05-19T17:28:48"
+        }
+    ]
+}
+
+export default PRODUCTS_V4;
